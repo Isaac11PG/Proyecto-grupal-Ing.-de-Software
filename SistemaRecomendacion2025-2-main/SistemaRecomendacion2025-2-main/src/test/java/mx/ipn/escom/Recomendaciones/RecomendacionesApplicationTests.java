957d5d79d2e352294f59d9215e57495e6cac664a
Isaac11PG <137459133+Isaac11PG@users.noreply.github.com>
2025-03-11 00:46:24 -0600

    Add files via upload

---
+++ SistemaRecomendacion2025-2-main/SistemaRecomendacion2025-2-main/src/test/java/mx/ipn/escom/Recomendaciones/RecomendacionesApplicationTests.java
@@ -0,0 +1,13 @@
+package mx.ipn.escom.Recomendaciones;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+import mx.ipn.escom.Recomendaciones.auth.RecomendacionesApplication;
+
+@SpringBootTest(classes = RecomendacionesApplication.class)
+class RecomendacionesApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+}
